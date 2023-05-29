52c7a17dec738732fd73b1a6fb5dbf3f0ef995cc
juan9129 <118020985+juan9129@users.noreply.github.com>
2023-05-29 20:51:22 +0000

    Deploying to gh-pages from @ juan9129/goit-js-hw-09@3f11f0c36b126ff07f464140f4fa1bfcd65af452 🚀

---
+++ 01-color-switcher.516255fc.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");var a=function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))};e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,a(),t=setInterval(a,1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.516255fc.js.map
